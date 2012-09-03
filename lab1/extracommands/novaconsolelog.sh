d373123ba9a7d9f06deaf7efdf1eb282c59d6617
nkadakia <nisha_rk1@yahoo.com>
2012-09-03 14:31:30 -0700

    Removed unwanted files

--- lab1/extracommands/novaconsolelog.sh
+++
@@ -1,3 +0,0 @@
-#It gets the output of the instance if you want to check the errors
-
-nova console-log
--- lab1/extracommands/novashow.sh
+++
@@ -1,3 +0,0 @@
-#This will display the details of the new server just built.
-# we can use its ID for next command.
-nova show <instancename>
